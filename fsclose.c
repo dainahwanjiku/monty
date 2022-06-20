c73a9db6c96e4b9403cb34a5e15d2a888648e7cb
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-06-20 05:33:55 +0300

    Update fsclose.c

--- fsclose.c
+++ fsclose.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 
 /**
- * m_fs_close - close file stream
+ * fs_close - close file stream
  * @status: status passed to exit
  * @arg: pointer to file stream
  *
  * Return: void
  */
-void m_fs_close(int status, void *arg)
+void fs_close(int status, void *arg)
 {
 	FILE *fs;
 
